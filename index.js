7bd6283c655c5a7dbd5e3f076d628cd3182daae9
Ketan Dhyani <ketandhyani@TTNs-MacBook-Pro.local>
2022-10-28 09:16:43 +0300

    second comkmit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import AssessmentOne  from './AssessmentOne';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => AssessmentOne);
